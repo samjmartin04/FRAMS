6a9bdaa784ae4d3d68e48a68f987eb3a3e2cdeec
samjmartin04 <sam_martin4@outlook.com>
2024-12-07 16:48:34 +0000

    Renamed 'add to existing schedule' to 'modify existing schedule' and allowed it to accept fewer teams that before, with the teams being removed able to be specified

--- RunFRAMS.py
+++ RunFRAMS.py
@@ -5,150 +5,20 @@ from CheckSchedule import *
 from CreateSchedule import *
 
 while True:
-    response = input("\nA  Add to existing schedule\nC  Check Schedule\nG  Generate Schedule\nO  Optimise Schedule\nS  Settings\n\nChoose option: ")
+    response = input("\nC  Check Schedule\nG  Generate Schedule\nM  Modify existing schedule\nO  Optimise Schedule\nS  Settings\n\nChoose option: ")
 
     if response == "":
         break
-    elif response.casefold()[:1] == "a":
-        print("")
+    elif response.casefold()[:1] == "c":
         found_schedule = False
         while not found_schedule:
-            old_schedule_location = input("Path of schedule to add to: ")
-            if os.path.isfile(old_schedule_location):
+            response = input("Please enter the path to the schedule: ")
+            if os.path.isfile(response):
                 found_schedule = True
-                match_schedule_old = import_schedule(old_schedule_location)
+                match_schedule = import_schedule(response)
+                interact_with_checks(match_schedule)
             else:
                 print("No schedule found at that location. Please try again.")
-        
-        truncate_schedule_check = True
-        while truncate_schedule_check:
-            response = input("Truncate schedule using (A)ppearance number, (M)atch Number, (N)o truncation? Response (A/M/N): ")
-            if response.casefold()[:1] == "a":
-                crop_appearances = int(input("Number of appearances before cut-off: "))
-
-                list_of_team_numbers = get_list_of_team_numbers_from_schedule(match_schedule_old)
-                success = False
-                n = 1
-                while n < len(match_schedule_old)+1 and not success:
-                    success_n = True
-                    cropped_schedule = match_schedule_old[:n]
-                    for specific_team_number in list_of_team_numbers:
-                        appearances_for_team = get_appearances_data(cropped_schedule, specific_team_number)
-                        if appearances_for_team != crop_appearances:
-                            success_n = False
-                            break
-                    if success_n:
-                        success = True
-                    else:
-                        n += 1
-                if success:
-                    match_schedule = [match_schedule_old[:n]]
-                    print(f"Successfully cut the existing schedule to have only {n} matches, which gives {crop_appearances} appearances per team.")
-                    truncate_schedule_check = False
-                else:
-                    print("Failed to cut the match schedule for that number of appearances. Please try a different number of appearances.")
-            elif response.casefold()[:1] == "m":
-                crop_match_num = int(input("Number of matches before cut-off: "))
-                list_of_team_numbers = get_list_of_team_numbers_from_schedule(match_schedule_old)
-                cropped_schedule = match_schedule_old[:crop_match_num]
-                appearances_dict = {}
-                for specific_team_number in list_of_team_numbers:
-                    appearances_for_team = get_appearances_data(cropped_schedule, specific_team_number)
-                    if appearances_for_team in appearances_dict.keys():
-                        appearances_dict[appearances_for_team] += 1
-                    else:
-                        appearances_dict[appearances_for_team] = 1
-                number_of_teams = get_number_of_teams_from_schedule(match_schedule_old)
-                success = False
-                for key in appearances_dict.keys():
-                    if appearances_dict[key] == number_of_teams:
-                        success = True
-                if success:
-                    print(f"Successfully cut the existing schedule to have only {crop_match_num} matches, which gives {key} appearances per team.")
-                    truncate_schedule_check = False
-                else:
-                    response_check = input(f"That number of matches gives an equal number of appearances per team. Do you want to continue? (Y/N): ")
-                    if response_check.casefold()[:1] == "y":
-                        print(f"Cut the existing schedule to have only {crop_match_num} matches, with an unequal number of appearances per team.")
-                        truncate_schedule_check = False
-                match_schedule = [match_schedule_old[:crop_match_num-1]]
-            elif response.casefold()[:1] == "n":
-                truncate_schedule_check = False
-                match_schedule = [match_schedule_old]
-
-        number_of_teams_int_check = True
-        while number_of_teams_int_check:
-            number_of_teams = input("Number of Teams: ")
-            try:
-                number_of_teams = int(number_of_teams)
-                number_of_teams_int_check = False
-            except:
-                print("Please enter a number.")
-        
-        number_of_appearances_int_check = True
-        while number_of_appearances_int_check:
-            number_of_appearances = input("Number of Appearances: ")
-            try:
-                number_of_appearances = int(number_of_appearances)
-                number_of_appearances_int_check = False
-            except:
-                print("Please enter a number.")
-        
-        schedule_priority = input("Would you like to prioritise (S)pacing, (B)alanced, (F)acings, or (C)ustom: ")
-        if schedule_priority == "":
-            schedule_priority = "b"
-        
-        if schedule_priority.casefold()[:1] == "b":
-            spacing_ratio = 0.5
-            facings_ratio = 0.5
-        elif schedule_priority.casefold()[:1] == "s":
-            spacing_ratio = 0.65
-            facings_ratio = 0.35
-        elif schedule_priority.casefold()[:1] == "f":
-            spacing_ratio = 0.35
-            facings_ratio = 0.65
-        elif schedule_priority.casefold()[:1] == "c":
-            response_spacing = input("What spacing ratio would you like: ")
-            response_facings = input("What facings ratio would you like: ")
-            try:
-                response_spacing = float(response_spacing)
-                response_facings = float(response_facings)
-                total = response_spacing + response_facings
-                spacing_ratio = response_spacing/total
-                facings_ratio = response_facings/total
-            except:
-                print("Error using your requested ratios, using balanced instead.")
-                spacing_ratio = 0.5
-                facings_ratio = 0.5
-        else:
-            spacing_ratio = 0.5
-            facings_ratio = 0.5
-        
-        speed_mode = 1
-
-        location_name = input("File/location for new schedule (must end in .txt): ")
-        if location_name == "":
-            location_name = f"{old_schedule_location[:-4]}_extended.txt"
-        
-        location = check_location(location_name)
-
-        program_start = int(time.time())
-        match_schedule = generate_schedule(number_of_teams, number_of_appearances, optimisation_level=speed_mode, silent=True, match_schedule=match_schedule)
-
-        match_schedule = remove_league_blocks_from_schedule(match_schedule)
-
-        export_schedule_to_txt(match_schedule, location, False)
-        schedule_score = int(round(get_overall_score_from_schedule(match_schedule, False),0))
-
-        program_end = int(time.time())
-        total_time = program_end-program_start
-        program_time_mins, program_time_secs = total_time//60, round((total_time/60 - total_time//60)*60, 0)
-        time_taken = f"{int(program_time_mins):02d}:{int(program_time_secs):02d}"
-        print(f"\nAdded {number_of_teams} teams, each with {number_of_appearances} appearances to schedule. This took {time_taken} (min:sec) to generate. Saved as {location}. Schedule score: {schedule_score}.")
-
-        response = input("Press enter to continue and view a summary of the checks...")
-
-        interact_with_checks(match_schedule)
     elif response.casefold()[:1] == "g":
         print("\nGenerate Schedule:\n")
         number_of_teams_int_check = True
@@ -236,16 +106,149 @@ while True:
         response = input("Press enter to continue and view a summary of the checks...")
 
         interact_with_checks(match_schedule)
-    elif response.casefold()[:1] == "c":
+    elif response.casefold()[:1] == "m":
+        print("")
         found_schedule = False
         while not found_schedule:
-            response = input("Please enter the path to the schedule: ")
-            if os.path.isfile(response):
+            old_schedule_location = input("Path of schedule to add to: ")
+            if os.path.isfile(old_schedule_location):
                 found_schedule = True
-                match_schedule = import_schedule(response)
-                interact_with_checks(match_schedule)
+                match_schedule_old = import_schedule(old_schedule_location)
             else:
                 print("No schedule found at that location. Please try again.")
+        
+        truncate_schedule_check = True
+        while truncate_schedule_check:
+            response = input("Truncate schedule using (A)ppearance number, (M)atch Number, (N)o truncation? Response (A/M/N): ")
+            if response.casefold()[:1] == "a":
+                crop_appearances = int(input("Number of appearances before cut-off: "))
+
+                list_of_team_numbers = get_list_of_team_numbers_from_schedule(match_schedule_old)
+                success = False
+                n = 1
+                while n < len(match_schedule_old)+1 and not success:
+                    success_n = True
+                    cropped_schedule = match_schedule_old[:n]
+                    for specific_team_number in list_of_team_numbers:
+                        appearances_for_team = get_appearances_data(cropped_schedule, specific_team_number)
+                        if appearances_for_team != crop_appearances:
+                            success_n = False
+                            break
+                    if success_n:
+                        success = True
+                    else:
+                        n += 1
+                if success:
+                    match_schedule = [match_schedule_old[:n]]
+                    print(f"Successfully cut the existing schedule to have only {n} matches, which gives {crop_appearances} appearances per team.")
+                    truncate_schedule_check = False
+                else:
+                    print("Failed to cut the match schedule for that number of appearances. Please try a different number of appearances.")
+            elif response.casefold()[:1] == "m":
+                crop_match_num = int(input("Number of matches before cut-off: "))
+                list_of_team_numbers = get_list_of_team_numbers_from_schedule(match_schedule_old)
+                cropped_schedule = match_schedule_old[:crop_match_num]
+                appearances_dict = {}
+                for specific_team_number in list_of_team_numbers:
+                    appearances_for_team = get_appearances_data(cropped_schedule, specific_team_number)
+                    if appearances_for_team in appearances_dict.keys():
+                        appearances_dict[appearances_for_team] += 1
+                    else:
+                        appearances_dict[appearances_for_team] = 1
+                number_of_teams = get_number_of_teams_from_schedule(match_schedule_old)
+                success = False
+                for key in appearances_dict.keys():
+                    if appearances_dict[key] == number_of_teams:
+                        success = True
+                if success:
+                    print(f"Successfully cut the existing schedule to have only {crop_match_num} matches, which gives {key} appearances per team.")
+                    truncate_schedule_check = False
+                else:
+                    response_check = input(f"That number of matches gives an equal number of appearances per team. Do you want to continue? (Y/N): ")
+                    if response_check.casefold()[:1] == "y":
+                        print(f"Cut the existing schedule to have only {crop_match_num} matches, with an unequal number of appearances per team.")
+                        truncate_schedule_check = False
+                match_schedule = [match_schedule_old[:crop_match_num-1]]
+            elif response.casefold()[:1] == "n":
+                truncate_schedule_check = False
+                match_schedule = [match_schedule_old]
+
+        number_of_teams_int_check = True
+        while number_of_teams_int_check:
+            number_of_teams = input("Number of Teams: ")
+            try:
+                number_of_teams = int(number_of_teams)
+                number_of_teams_int_check = False
+            except:
+                print("Please enter a number.")
+            
+        teams_to_exclude_text = input("Team numbers to exclude: ")
+        teams_to_exclude = number_array_from_list(teams_to_exclude_text)
+        
+        number_of_appearances_int_check = True
+        while number_of_appearances_int_check:
+            number_of_appearances = input("Number of Appearances: ")
+            try:
+                number_of_appearances = int(number_of_appearances)
+                number_of_appearances_int_check = False
+            except:
+                print("Please enter a number.")
+        
+        schedule_priority = input("Would you like to prioritise (S)pacing, (B)alanced, (F)acings, or (C)ustom: ")
+        if schedule_priority == "":
+            schedule_priority = "b"
+        
+        if schedule_priority.casefold()[:1] == "b":
+            spacing_ratio = 0.5
+            facings_ratio = 0.5
+        elif schedule_priority.casefold()[:1] == "s":
+            spacing_ratio = 0.65
+            facings_ratio = 0.35
+        elif schedule_priority.casefold()[:1] == "f":
+            spacing_ratio = 0.35
+            facings_ratio = 0.65
+        elif schedule_priority.casefold()[:1] == "c":
+            response_spacing = input("What spacing ratio would you like: ")
+            response_facings = input("What facings ratio would you like: ")
+            try:
+                response_spacing = float(response_spacing)
+                response_facings = float(response_facings)
+                total = response_spacing + response_facings
+                spacing_ratio = response_spacing/total
+                facings_ratio = response_facings/total
+            except:
+                print("Error using your requested ratios, using balanced instead.")
+                spacing_ratio = 0.5
+                facings_ratio = 0.5
+        else:
+            spacing_ratio = 0.5
+            facings_ratio = 0.5
+        
+        speed_mode = 1
+
+        location_name = input("File/location for new schedule (must end in .txt): ")
+        if location_name == "":
+            location_name = f"{old_schedule_location[:-4]}_modified.txt"
+        
+        location = check_location(location_name)
+
+        program_start = int(time.time())
+        match_schedule = generate_schedule(number_of_teams, number_of_appearances, optimisation_level=speed_mode, silent=True, match_schedule=match_schedule, teams_to_exclude=teams_to_exclude)
+
+        match_schedule = remove_league_blocks_from_schedule(match_schedule)
+
+        export_schedule_to_txt(match_schedule, location, False)
+        schedule_score = int(round(get_overall_score_from_schedule(match_schedule, False),0))
+
+        program_end = int(time.time())
+        total_time = program_end-program_start
+        program_time_mins, program_time_secs = total_time//60, round((total_time/60 - total_time//60)*60, 0)
+        time_taken = f"{int(program_time_mins):02d}:{int(program_time_secs):02d}"
+        print(f"\nAdded {number_of_teams} teams, each with {number_of_appearances} appearances to schedule. This took {time_taken} (min:sec) to generate. Saved as {location}. Schedule score: {schedule_score}.")
+
+        response = input("Press enter to continue and view a summary of the checks...")
+
+        interact_with_checks(match_schedule)
     elif response.casefold()[:1] == "o":
         found_schedule = False
         while not found_schedule:
